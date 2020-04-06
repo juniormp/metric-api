51d0d0732a8190697e232735974589c5cbbb91c0
Maurício Junior <junior9771@gmail.com>
2020-04-06 19:48:36 -0300

    create method to get metric list on repository

--- src/infrastructure/repository/metricRepository.go
+++ src/infrastructure/repository/metricRepository.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"errors"
+
 	"github.com/juniormp/metric-api/src/domain"
 )
 
@@ -8,4 +10,27 @@ func (redisAdapter RedisAdapter) AddMetricRepository(metric domain.Metric) error
 	_, err := redisAdapter.Client.LPush(string(metric.Name), metric.ExpiredAt).Result()
 
 	return err
+}
+
+func (redisAdapter RedisAdapter) ListMetricsRepository(metricName string) (domain.Metrics, error) {
+	var metrics domain.Metrics
+	qtdMetrics, err := redisAdapter.Client.LLen(metricName).Result()
+
+	if err != nil {
+		return metrics, errors.New("Error while getting metric list")
+	}
+
+	if qtdMetrics == 0 {
+		return metrics, errors.New("Metric do not hava values")
+	}
+
+	values, err := redisAdapter.Client.LRange(metricName, 0, qtdMetrics).Result()
+
+	if err != nil {
+		return metrics, errors.New("Error while getting metric list")
+	}
+
+	metrics = domain.Metrics{metricName, values}
+
+	return metrics, err
 }
--- tests/infrastructure/repository/metricRepository_test.go
+++ tests/infrastructure/repository/metricRepository_test.go
@@ -7,11 +7,13 @@ import (
 	"github.com/go-redis/redis"
 	"github.com/juniormp/metric-api/src/domain"
 	"github.com/juniormp/metric-api/src/infrastructure/repository"
+	factorytest "github.com/juniormp/metric-api/tests/factory"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreatesAMetric(t *testing.T) {
+func TestCreatesMetric(t *testing.T) {
 	client := redis.NewClient(&redis.Options{Addr: os.Getenv("REDIS_SERVER")})
+	client.FlushAll()
 	redisAdapter := repository.RedisAdapter{client}
 	metric := domain.Metric{
 		Name:      "clicks",
@@ -24,5 +26,29 @@ func TestCreatesAMetric(t *testing.T) {
 
 	assert.Equal(t, metric.ExpiredAt, validate, "it creates a metric into a redis list")
 	assert.Empty(t, err)
+	client.FlushAll()
+}
+
+func TestGetMetricList(t *testing.T) {
+	metrics := domain.Metrics{
+		Name: "clicks",
+		Values: []string{
+			"2020-04-05T15:04:05Z07:00_20",
+			"2020-04-05T16:04:05Z07:00_20",
+			"2020-04-05T17:04:05Z07:00_20",
+		},
+	}
+	client := redis.NewClient(&redis.Options{Addr: os.Getenv("REDIS_SERVER")})
+	redisAdapter := repository.RedisAdapter{client}
+	factorytest.PersistMetrics(metrics, redisAdapter)
+
+	response, _ := redisAdapter.ListMetricsRepository("clicks")
+
+	assert.Equal(t, metrics.Name, response.Name)
+	assert.Equal(t, metrics.Values[0], response.Values[2])
+	assert.Equal(t, metrics.Name, response.Name)
+	assert.Equal(t, metrics.Values[1], response.Values[1])
+	assert.Equal(t, metrics.Name, response.Name)
+	assert.Equal(t, metrics.Values[2], response.Values[0])
 	client.FlushAll()
 }
