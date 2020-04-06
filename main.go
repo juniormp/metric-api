f87ecc19123c476a6ea637baf4fbdb6b4a9b14b7
Maurício Junior <junior9771@gmail.com>
2020-04-06 15:39:32 -0300

    create add metric endpoint

--- main.go
+++ main.go
@@ -4,14 +4,19 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/juniormp/metric-api/src/application/usecases"
+	"github.com/juniormp/metric-api/src/middleware"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(middleware.RedisHandler)
 
 	router.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"status": "live!"})
 	})
+
+	router.POST("/metric/:name", usecases.AddMetric)
 
 	router.Run()
 }
