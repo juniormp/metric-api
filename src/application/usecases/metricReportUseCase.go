671f7429a3f1f2f63a937c2cf6f4d9e814e6e267
Maurício Junior <junior9771@gmail.com>
2020-04-06 19:53:49 -0300

    create report metric usecase

---
+++ src/application/usecases/metricReportUseCase.go
@@ -0,0 +1,35 @@
+package usecases
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+	"github.com/juniormp/metric-api/src/domain"
+	"github.com/juniormp/metric-api/src/infrastructure/repository"
+)
+
+func MetricReport(c *gin.Context) {
+	metricName := c.Param("name")
+
+	report := executeMetricReport(metricName)
+
+	c.JSON(http.StatusOK, gin.H{
+		"value": report,
+	})
+}
+
+func executeMetricReport(metricName string) int {
+	client := redis.NewClient(&redis.Options{Addr: os.Getenv("REDIS_SERVER")})
+	redisAdapter := repository.RedisAdapter{client}
+	metrics, err := redisAdapter.ListMetricsRepository(metricName)
+
+	if err != nil {
+		return 0
+	}
+
+	report := domain.CreateMetricReport(metrics)
+
+	return report
+}
