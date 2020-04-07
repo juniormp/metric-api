2db469c535c3750e558718ac1839ee2991d6a988
Maurício Junior <junior9771@gmail.com>
2020-04-07 01:03:09 -0300

    create clean metrics usecase

---
+++ src/application/usecases/cleanMetricsUsecase.go
@@ -0,0 +1,19 @@
+package usecases
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+	"github.com/juniormp/metric-api/src/infrastructure"
+	"github.com/juniormp/metric-api/src/infrastructure/repository"
+)
+
+func CleanMetrics(c *gin.Context) {
+	client := redis.NewClient(&redis.Options{Addr: os.Getenv("REDIS_SERVER")})
+	redisAdapter := repository.RedisAdapter{client}
+	infrastructure.CleanMetrics(redisAdapter)
+
+	c.JSON(http.StatusCreated, "")
+}
