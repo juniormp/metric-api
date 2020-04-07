f7c81c461c1a365b1d6d3ef1a001d69c1a553913
Maurício Junior <junior9771@gmail.com>
2020-04-07 00:58:15 -0300

    add helper method on factory

--- tests/factory/metricFactory.go
+++ tests/factory/metricFactory.go
@@ -5,6 +5,10 @@ import (
 	"github.com/juniormp/metric-api/src/infrastructure/repository"
 )
 
+func PersistMetric(metric domain.Metric, redisAdapter repository.RedisAdapter) {
+	redisAdapter.Client.LPush(metric.Name, metric.ExpiredAt).Result()
+}
+
 func PersistMetrics(metrics domain.Metrics, redisAdapter repository.RedisAdapter) {
 	for _, expiredAt := range metrics.Values {
 		redisAdapter.Client.LPush(metrics.Name, expiredAt).Result()
